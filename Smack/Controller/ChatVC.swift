1e47ee21372e3a0bc788a07c24d936c90cb4475c
Leslie <leslie.appl@gmail.com>
2019-08-16 17:37:39 +0800

    SWReveal

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,36 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by leslie on 8/16/19.
+//  Copyright © 2019 leslie. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer((self.revealViewController()?.panGestureRecognizer())!)
+        self.view.addGestureRecognizer((self.revealViewController()?.tapGestureRecognizer())!)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
