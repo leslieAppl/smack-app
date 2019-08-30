00a55eb7c1a6aa16cf8b6d0ac92f9b6b91ee9285
Leslie <leslie.appl@gmail.com>
2019-08-30 14:12:09 +0800

    Add Channel VC

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -30,6 +30,12 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         setupUserInfo()
     }
     
+    @IBAction func addChannelBtn(_ sender: Any) {
+        let addChannel = AddChannelVC()
+        addChannel.modalPresentationStyle = .custom
+        present(addChannel, animated: true, completion: nil)
+    }
+    
     @IBAction func unwindFromCreateAccountVC(unwindSegue: UIStoryboardSegue) {}
     
     @IBAction func loginBtn(_ sender: Any) {
